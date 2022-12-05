ce9291f517b235e9afaee85f6453d11fd642c68f
msnbek <mahmutsenbek@icloud.com>
2022-12-06 00:01:13 +0300

    basic version

---
+++ TRNewsAPICallApp/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  TRNewsAPICallApp
+//
+//  Created by Mahmut Senbek on 5.12.2022.
+//
+
+import Foundation
---
+++ TRNewsAPICallApp/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  TRNewsAPICallApp
+//
+//  Created by Mahmut Senbek on 5.12.2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

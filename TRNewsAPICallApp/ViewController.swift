3ce706c016fadb8cbba42cca9812e6e98e76c088
msnbek <mahmutsenbek@icloud.com>
2022-12-05 22:01:23 +0300

    Initial Commit

---
+++ TRNewsAPICallApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TRNewsAPICallApp
+//
+//  Created by Mahmut Senbek on 5.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
